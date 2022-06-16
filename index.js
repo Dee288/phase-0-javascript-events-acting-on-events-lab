87adabcd3fbf9dabc666f98592e751e7568d18bc
dee288 <dianamajala10@gmail.com>
2022-06-16 20:18:44 +0300

    commit tree c7235c132b3fe70f17c10594f50e12edc3bf62bf

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function moveDodgerRight() {
   const leftNumbers = dodger.style.left.replace('px', '');
   const left = parseInt(leftNumbers, 10)
   if (left < 360) {
-    dodger.style.left = `${left + 1}px`
+    dodger.style.left= `${left + 1}px`
   }
 }
 
